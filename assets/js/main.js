f6fa7c22b6ef73d27a969f230c92c26f47c21596
Oleg <olegslyvka@gmail.com>
2017-06-08 14:06:11 +0300

    Init

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+$(function(){
+	carousel($('.slider__list'), $('.slider__arrow_left'), 
+	      	 $('.slider__arrow_right'),  $('.slider__item'), 
+	      	 $('input[name="slider__radio"]'), $('.slider__main'));
+});
