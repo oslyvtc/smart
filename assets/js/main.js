2a9ceab9c96c51730aa665cf8a442fe0eea51391
Oleg <olegslyvka@gmail.com>
2017-06-09 05:27:19 +0300

    DOne

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,38 @@ $(function(){
 		});
 	});
 
+	$('.nav-top__burger').click(function(){
+		$('.nav-top__list-mob').toggle(1000)
+	});
+
+	$('.blog-js').click(function(e) {
+		e.preventDefault();
+		$('.nav-middle-nested__list').toggle(1000)
+	})
+
+	
+	setInterval(function(){
+		$('.back').animate({
+			margin: '20px 20px 0 20px'
+		}, 2000, function(){
+			$('.back').css({
+				margin: '20px 20px 0 50px'
+			})
+		})
+	},2000)
+
+	$('.back').click(function(e){
+		e.preventDefault();
+		$('.nav-middle-nested__list').toggle(1000)
+	})
+	
+	
+	
+
+
+
+
+
 	carousel($('.slider__list'), $('.slider__arrow_left'), 
 	      	 $('.slider__arrow_right'),  $('.slider__item'), 
 	      	 $('input[name="slider__radio"]'), $('.slider__main'));
